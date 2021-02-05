a089e1e1f258cf6b1ae571cf20c53558302f7946
RosemaryLy <rosey.ly@hotmail.com>
2021-02-05 03:30:17 -0500

    Updates

---
+++ precache-manifest.2703efac2535393369ddb4236c7b2fb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162ba3ce4e863f6df2d9ab5a4b0cbbfb",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "409f9d059c1d53b9caf8",
+    "url": "/React-Weather-App/static/css/main.d5b7a424.chunk.css"
+  },
+  {
+    "revision": "2042f4a317d864c8a34f",
+    "url": "/React-Weather-App/static/js/2.a9465ece.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.a9465ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "409f9d059c1d53b9caf8",
+    "url": "/React-Weather-App/static/js/main.1207f86a.chunk.js"
+  },
+  {
+    "revision": "553eabf34a58b6774377",
+    "url": "/React-Weather-App/static/js/runtime-main.6fe5135b.js"
+  },
+  {
+    "revision": "ca1aebc693487d0dc2f77244f5d8d303",
+    "url": "/React-Weather-App/static/media/Torontocityscape.ca1aebc6.jpg"
+  }
+]);
